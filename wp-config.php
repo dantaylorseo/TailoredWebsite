ded1a88510ded3a5b31fcdf182a096435b77bae6
dantaylorseo <dan@tailored.im>
2014-10-27 10:22:37 +0000

    Test

--- wp-config.php
+++ wp-config.php
@@ -35,7 +35,7 @@ define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
+// Test
 /**#@+
  * Authentication Unique Keys and Salts.
  *
